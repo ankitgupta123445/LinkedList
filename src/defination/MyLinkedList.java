dd8b590c297eaa012650f2009f8c6e488bc89d91
ankitgupta123445 <ankit.gla_cs18@gla.ac.in>
2020-01-23 11:05:34 +0530

    make node

--- src/defination/MyLinkedList.java
+++ src/defination/MyLinkedList.java
@@ -3,13 +3,39 @@ package defination;
 import adt.LinkedListADT;
 
 public class MyLinkedList<E> implements LinkedListADT {
+    private Node head = null;
+    private int size = 0;
+
+    private void addFirst(E item) {
+        head = new Node<>(item, head);
+        size++;
+    }
+
+    private class Node<E> {
+        private E data;
+        private Node<E> next;
+
+        public Node(E data) {
+            this.data = data;
+        }
+
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+    }
     @Override
     public void add(int index, Object item) {
 
     }
 
-    private class Node<E> {
-        private E data;
-        private Node node;
-    }
+
 }
